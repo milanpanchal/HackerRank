0c5304d2b18b72d65d37fe77bbf0e77d34a9a6c8
Milan Panchal <sam07it22@gmail.com>
2020-04-01 21:36:06 +0530

    Problem statement added for Priyanka & Toys problem

--- Problem Solving/Greedy/PriyankaAndToys.playground/Contents.swift
+++ Problem Solving/Greedy/PriyankaAndToys.playground/Contents.swift
@@ -1,6 +1,40 @@
+/*:
+ # Priyanka and Toys
+ [HackerRank Problem - Priyanka and Toys](https://www.hackerrank.com/challenges/priyanka-and-toys/problem "HackerRank Problem - Priyanka and Toys")
+
+ ---
+ 
+ ### Problem Statement
+ Priyanka works for an international toy company that ships by container. Her task is to the determine the lowest cost way to combine her orders for shipping. She has a list of item weights. The shipping company has a requirement that all items loaded in a container must weigh less than or equal to 4 units plus the weight of the minimum weight item. All items meeting that requirement will be shipped in one container.
+ 
+ What is the smallest number of containers that can be contracted to ship the items based on the given list of weights?
+
+ For example, there are items with weights w = [1, 2, 3, 4, 5, 10, 11, 12, 13] . This can be broken into two containers: [1, 2, 3, 4, 5]  and [10, 11, 12, 13]. Each container will contain items weighing within 4 units of the minimum weight item.
+
+ ### Function Description
+ Complete the toys function in the editor below. It should return the minimum number of containers required to ship.
+
+ toys has the following parameter(s):
+ * w: an array of integers that represent the weights of each order to ship
+ 
+ ### Input Format
+ 
+ The first line contains an integer n, the number of orders to ship.
+ The next line contains n space-separated integers, w[1], w[2],...,w[n], representing the orders in a weight array.
+ 
+ ### Constraints
+ 1 ≤ n ≤ 10^5
+ 
+ 0 ≤ w[i] ≤ 10^4, where i ∈ [1,n]
+
+
+ ### Output Format
+ Return the integer value of the number of containers Priyanka must contract to ship all of the toys.
+ 
+*/
+
 import Foundation
 
-//https://www.hackerrank.com/challenges/priyanka-and-toys/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
 
 // Complete the toys function below.
 func toys(w: [Int]) -> Int {
