d3a839df643f930d7d8bf25326752ec609e8f15a
Milan Panchal <sam07it22@gmail.com>
2020-04-06 15:26:26 +0530

    fix: Bon Appetit

--- Problem Solving/Implementation/BonAppétit.playground/Contents.swift
+++ Problem Solving/Implementation/BonAppétit.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+/*:
+# Bon Appétit
 
-var str = "Hello, playground"
+[HackerRank Problem - Bon Appétit](https:www.hackerrank.com/challenges/bon-appetit/problem "HackerRank Problem - Bon Appétit")
+
+---
+ */
+
+import Foundation
+
+// Complete the bonAppetit function below.
+func bonAppetit(bill: [Int], k: Int, b: Int) -> Void {
+
+    let totalSum = bill.reduce(0, +)
+    let annaActualcontribution = (totalSum - bill[k])/2
+    if b == annaActualcontribution {
+        print("Bon Appetit")
+    } else {
+        print(b-annaActualcontribution)
+    }
+
+}
